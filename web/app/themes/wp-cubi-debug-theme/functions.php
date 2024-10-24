75ce8e21783e034beac3155d2eb8f90f2072f383
Aidenone <aidenone.lucas@gmail.com>
2024-10-25 01:34:31 +0200

    bug fix 2

--- web/app/themes/wp-cubi-debug-theme/functions.php
+++ web/app/themes/wp-cubi-debug-theme/functions.php
@@ -2,3 +2,10 @@
 
 require_once __DIR__ . '/src/schema.php';
 require_once __DIR__ . '/src/registrations.php';
+
+
+if(preg_match('/\/registrations\/(.*)/', $_SERVER['REQUEST_URI']) > 0) {
+	header('HTTP/1.1 404 Not Found');
+	$_GET['e'] = 404;
+	exit;
+}

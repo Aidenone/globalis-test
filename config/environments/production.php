0e1002820a9ddcdb2367f1479d981d051e97a5d6
Aidenone <aidenone.lucas@gmail.com>
2024-10-25 00:19:15 +0200

    first commit

--- config/environments/production.php
+++ config/environments/production.php
@@ -2,7 +2,7 @@
 
 /* DEBUG */
 ini_set('display_errors', 0);
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('SAVEQUERIES', false);
